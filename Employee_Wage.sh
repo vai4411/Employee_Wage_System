b8339b3ceb9d6971addb7f7806f7fb695d513aba
root <root@localhost.localdomain>
2020-04-13 16:00:59 +0000

    ADDED: Check Employee Present Or Absent

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,2 +1,17 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage System"
+
+#variable
+attendance=1
+present=0
+
+function employeeAttendance(){
+	present=$((RANDOM % 2))
+	if [ $present -eq $attendance ]
+	then
+		echo "Employee is Present"
+	else
+		echo "Employee is Absent"
+	fi
+}
+employeeAttendance
