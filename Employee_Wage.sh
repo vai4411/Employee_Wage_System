f2c2114fffa56240227a813fdb0b3c57217c2067
root <root@localhost.localdomain>
2020-04-13 16:39:25 +0000

    ADDED: Add Part Time Employee & Wage

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -4,22 +4,33 @@ echo "Welcome to Employee Wage System"
 #constant
 wagePerHour=20
 fullDayHour=8
+partTimeHour=8
 
 #variable
 attendance=1
 present=0
 dailyWage=0
+partTimeEmployee=""
+partTimeWage=0
 
 function employeeAttendance() {
 	present=$((RANDOM % 2))
 	if [ $present -eq $attendance ]
 	then
 		echo "Employee is Present"
-		dailyWage=$(($wagePerHour * $fullDayHour))
 	else
 		echo "Employee is Absent"
-		dailyWage=0
 	fi
+}
+
+function employeeDailyWage() {
+	dailyWage=$(($wagePerHour * $fullDayHour))
 	echo "Employee Daily Wage is $dailyWage"
 }
-employeeAttendance
+
+function employeePartTimeWage() {
+	read -p "Enter part time employee name:" partTimeEmployee
+	partTimeWage=$(($wagePerHour * $fullDayHour / 2))
+	echo "$partTimeEmployee has $partTimeWage part time wage"
+}
+employeePartTimeWage
