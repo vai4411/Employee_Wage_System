779326b88633e68952a0198060a648371065a3a4
root <root@localhost.localdomain>
2020-04-13 16:21:37 +0000

    ADDED: Calculate Daily Employee Wage

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage System"
 
+#constant
+wagePerHour=20
+fullDayHour=8
+
 #variable
 attendance=1
 present=0
+dailyWage=0
 
-function employeeAttendance(){
+function employeeAttendance() {
 	present=$((RANDOM % 2))
 	if [ $present -eq $attendance ]
 	then
 		echo "Employee is Present"
+		dailyWage=$(($wagePerHour * $fullDayHour))
 	else
 		echo "Employee is Absent"
+		dailyWage=0
 	fi
+	echo "Employee Daily Wage is $dailyWage"
 }
 employeeAttendance
